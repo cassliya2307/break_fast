5625ce2c48df2470fd5bc1c14e7595596fb4918a
cassliya2307 <haliyakareem23@gmail.com>
2025-10-15 18:57:01 +0100

    Add JS Practice.js

---
+++ JS Practice/stringAppearance.js
@@ -0,0 +1,10 @@
+let counter = 0;
+let sum = 0;
+let string = "elephant"
+for(counter = 1; counter <= string.length; counter++){
+if(counter == "e" || counter == "E"){
+let sum = sum + 1;
+
+}
+console.log(sum)
+}
